d2740a643e1dd38318f53a59ac91917f1aa13053
ZoraGK <jyh199759@126.com>
2018-12-29 16:32:09 +0800

    读文件
    完成了读取文件并进行格式化功能。
    完成了字符串转集合的功能。

--- 词频分析/work1/work1.py
+++ 词频分析/work1/work1.py
@@ -1,15 +1,42 @@
 # work1.py
-rd=open("input_1.txt","r")
-ls=rd.read()
-rd.close()
-# 格式化
-ls=ls.lower()
-ls=ls.replace(".\n","")
-ls=ls.replace(".","")
-ls=ls.replace(",","")
-ls=ls.split(" ")
-print(ls)
-'''word=[]
-target=open("testTarget.txt","w")
-target.write('\n'.join(word))
-target.close()'''
+def readfile(fname):#读文件
+    try:
+        f=open(fname, "r")
+        ls=f.read()
+        f.close()
+        return ls
+    except all:
+        print("readfile error")
+        exit()
+
+def txtformat(ls):# 格式化
+    ls=ls.lower()
+    ls=ls.replace(".\n","")
+    ls=ls.replace(".","")
+    ls=ls.replace(",","")
+    ls=ls.split(" ")
+    return ls
+
+def gettxt(fname):# 封装:读文件与格式化
+    return txtformat(readfile(fname))
+
+def getagg(ls):#获取集合
+    agg=set(ls)
+    # print(agg1)
+    return agg
+
+def writefile(fname,txt):#写入文件
+    f=open(fname,"w")
+    # TODO : f.writelines or f.write
+    f.write(txt)
+    f.write('\n')
+    f.close()
+
+if __name__ == '__main__':
+    fname="input_1.txt"
+    txt1=gettxt(fname)
+    fname="input_2.txt"
+    txt2=gettxt(fname)
+    agg1=getagg(txt1)
+    
+    # print(txt1)
