01e640160cf7609d790f24306a09cf141b1fc2ef
ZoraGK <jyh199759@126.com>
2018-12-29 17:19:43 +0800

    查重功能

--- 词频分析/work1/work1.py
+++ 词频分析/work1/work1.py
@@ -1,8 +1,10 @@
 # work1.py
-def readfile(fname):#读文件
+def readfile(fname):#读文件并初始化输出文件
     try:
         f=open(fname, "r")
         ls=f.read()
+        f.close()
+        f=open("output.txt","w")
         f.close()
         return ls
     except all:
@@ -26,18 +28,22 @@ def getagg(ls):# 获取集合
     # print(agg1)
     return agg
 
-def writefile(fname,txt):# 写入文件
-    f=open(fname,"w")
+def writefile(fname,ls):# 写入文件
+    f=open(fname,"a")
     # TODO : f.writelines or f.write
-    f.write(txt)
+    f.writelines(ls)
     f.write('\n')
     f.close()
 
-def countdifferent(t1,t2):# 查两字符串不同词的数量
-    pass
+def countsame(s,cmp):# 查两字符串相同词的数量
+    count=0;
+    for item in s:
+        if(item in cmp):
+            count+=1
+    return count
 
-def countsame(nout,nall):# 查相同单词数，nall总数-nout独有单词数=相同单词数
-    pass
+def countdiff(nall,nsame):# 查不同单词数，nall总数-nsame相同单词数=不同单词数
+    return nall-nsame
 
 if __name__ == '__main__':
     fname="input_1.txt"
@@ -45,5 +51,7 @@ if __name__ == '__main__':
     fname="input_2.txt"
     txt2=gettxt(fname)
     agg1=getagg(txt1)
-    
-    print(txt1)
+    agg2=getagg(txt2)
+    ct=50000
+    print("same word =", countsame(agg1,agg2))
+    # print(txt1)
