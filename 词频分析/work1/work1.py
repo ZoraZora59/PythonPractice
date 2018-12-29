9c57f0168453ea3d8fe1bafb8ab57ca8506a6139
ZoraGK <jyh199759@126.com>
2018-12-29 16:47:09 +0800

    Update work1.py

--- 词频分析/work1/work1.py
+++ 词频分析/work1/work1.py
@@ -11,6 +11,7 @@ def readfile(fname):#读文件
 
 def txtformat(ls):# 格式化
     ls=ls.lower()
+    ls=ls.replace("—"," ")
     ls=ls.replace(".\n","")
     ls=ls.replace(".","")
     ls=ls.replace(",","")
@@ -20,17 +21,23 @@ def txtformat(ls):# 格式化
 def gettxt(fname):# 封装:读文件与格式化
     return txtformat(readfile(fname))
 
-def getagg(ls):#获取集合
+def getagg(ls):# 获取集合
     agg=set(ls)
     # print(agg1)
     return agg
 
-def writefile(fname,txt):#写入文件
+def writefile(fname,txt):# 写入文件
     f=open(fname,"w")
     # TODO : f.writelines or f.write
     f.write(txt)
     f.write('\n')
     f.close()
+
+def countdifferent(t1,t2):# 查两字符串不同词的数量
+    pass
+
+def countsame(nout,nall):# 查相同单词数，nall总数-nout独有单词数=相同单词数
+    pass
 
 if __name__ == '__main__':
     fname="input_1.txt"
@@ -39,4 +46,4 @@ if __name__ == '__main__':
     txt2=gettxt(fname)
     agg1=getagg(txt1)
     
-    # print(txt1)
+    print(txt1)
